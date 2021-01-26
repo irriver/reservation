3866a91a9dcd7a4c91a748225419358613f0acd5
irriver <maraqann@gmail.com>
2021-01-26 20:04:53 +0900

    오류 수정: 응답 결과에 count 값 제대로 나오도록

--- src/main/java/kr/or/connect/reservation/dao/category/CategoryDaoSql.java
+++ src/main/java/kr/or/connect/reservation/dao/category/CategoryDaoSql.java
@@ -1,10 +1,10 @@
 package kr.or.connect.reservation.dao.category;
 
 public class CategoryDaoSql {
-	public static final String SELECT_ALL =  "SELECT category.id, "
+	public static final String SELECT_ALL = "SELECT category.id, "
 													+ "category.name, "
 													+ "(SELECT count(*) "
 													+ "FROM product "
-													+ "WHERE category.id = product.category_id) "
+													+ "WHERE category.id = product.category_id) AS count "
 											+ "FROM category";
 }
